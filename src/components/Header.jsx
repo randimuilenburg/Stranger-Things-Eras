cb754c0cdbafb190265e69222e17a997e35388a6
DingoDev <randilea@outlook.com>
2025-12-17 16:16:17 -0700

    Adding objects, css

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="header">
+        Select a Stranger Things character, get a Taylor Swift Era!
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/data/CharacterData.jsx
@@ -0,0 +1,74 @@
+const characterData = [
+  {
+    id: 1,
+    name: "Mike Wheeler",
+    era: "1989",
+    color: "#83b9eeff",
+  },
+  {
+    id: 2,
+    name: "Eleven",
+    era: "The Life of a Showgirl",
+    color: "#f73e05f3",
+  },
+  {
+    id: 3,
+    name: "Will Byers",
+    era: "Midnights",
+    color: "#030247f3",
+  },
+  {
+    id: 4,
+    name: "Dustin Henderson",
+    era: "Lover",
+    color: "#f54b8cf3",
+  },
+  {
+    id: 5,
+    name: "Lucas Sinclair",
+    era: "Speak Now",
+    color: "#780aacf3",
+  },
+  {
+    id: 6,
+    name: "Max Mayfield",
+    era: "Reputation",
+    color: "#0c0c0cff",
+  },
+  {
+    id: 7,
+    name: "Steve Harrington",
+    era: "Red",
+    color: "#c90909ff",
+  },
+  {
+    id: 8,
+    name: "Nancy Wheeler",
+    era: "folklore",
+    color: "#777272ff",
+  },
+  {
+    id: 9,
+    name: "Jonathan Byers",
+    era: "Taylor Swift (Debut/Self-Titled)",
+    color: "#1ce73eff",
+  },
+  {
+    id: 10,
+    name: "Robin Buckley",
+    era: "Fearless",
+    color: "#bbb806ff",
+  },
+  {
+    id: 11,
+    name: "Billy Hargrove",
+    era: "The Tortured Poets Department",
+    color: "#ffffffff",
+  },
+  {
+    id: 12,
+    name: "Eddie Munson",
+    era: "evermore",
+    color: "#b86b3fff",
+  },
+];
