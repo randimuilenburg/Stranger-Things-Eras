2c25fc0170e666ad73115ad309c0451a500071f3
DingoDev <randilea@outlook.com>
2025-12-17 17:40:39 -0700

    Objects built out, images added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
       <h1 className="header">
-        Select a Stranger Things character, get a Taylor Swift Era!
+        Select a Stranger Things character, get a Taylor Swift Era:
       </h1>
     </div>
   );
