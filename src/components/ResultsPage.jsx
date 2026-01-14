c17138af14a7d08f135058bce057344bdd110875
DingoDev <randilea@outlook.com>
2026-01-13 18:05:17 -0700

    Added songs, adjusted CSS

--- src/components/ResultsPage.jsx
+++ src/components/ResultsPage.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 import characterData from "../data/CharacterData";
 import ImageGrid from "./ImageGrid";
 
 function ResultsPage() {
-  return <h1 className="era-header">ERA</h1>;
+  const { slug } = useParams();
+
+  const character = characterData.find((character) => character.slug === slug);
+
+  return (
+    <div>
+      <h1 className="results-header" style={{ color: character.color }}>
+        {character.era}
+      </h1>
+      <img className="results-image" src={character.photo} />
+      <ul className="results-songs">
+        {character.songs.map((song) => (
+          <li className="single-song">{song}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ResultsPage;
